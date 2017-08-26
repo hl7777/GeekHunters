79d0360b2e09c9900b8e7d61dbc1fa73b02b1194
hl7777 <hl7777@hotmail.com>
2017-08-27 10:29:32 +1200

    Update AccountResponsitory.cs

--- GeekHunter.Data.Implementation/Responsitories/AccountResponsitory.cs
+++ GeekHunter.Data.Implementation/Responsitories/AccountResponsitory.cs
@@ -11,18 +11,15 @@ namespace GeekHunter.Data.Implementation.Responsitories
 
         public bool Login(string name, string password)
         {
-            using (GeekHunterEntities ctx = new GeekHunterEntities())
+            var user = ctx.AdminUsers.FirstOrDefault(x => x.UserName == name && x.Password == password);
+            if (user == null)
             {
-                var user = ctx.AdminUsers.FirstOrDefault(x => x.UserName == name && x.Password == password);
-                if (user == null)
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
+                return false;
             }
+            else
+            {
+                return true;
+            }            
         }
     }
 }
